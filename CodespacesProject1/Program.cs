29588be2bf894af81921e7a28a5d0f1677aba463
Dennis <122607995+Kigath1@users.noreply.github.com>
2023-11-20 13:16:52 +0000

    first commit for the hallo world code

---
+++ CodespacesProject1/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello Github spaces");
+Console.WriteLine("i have just introduced myself to C# on git spaces");
